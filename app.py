f17e9c1da0570ec3839ce75a807639ebece8d1ad
Vamshi-Krishna-Prime <vamshi.krishna.prime@gmail.com>
2021-07-21 16:28:28 +0530

    Add: webapp using streamlit

---
+++ app.py
@@ -0,0 +1,12 @@
+# import relevant packages
+import streamlit as st
+from streamlit.hashing import _CodeHasher
+import cv2
+import numpy as np
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import time
+import math
