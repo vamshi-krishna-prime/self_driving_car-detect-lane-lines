bce4d9f0f6c32d1ae0ac6b25380d98a055e0b1ff
Vamshi-Krishna-Prime <vamshi.krishna.prime@gmail.com>
2021-07-21 16:52:35 +0530

    Edit: app.py, initialize the reqs

--- app.py
+++ app.py
@@ -10,3 +10,34 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import time
 import math
+
+st.set_page_config(
+    page_title="Self Driving Car ND", # String or None. Strings get appended with "• Streamlit". 
+    page_icon=":blue_car:", # String, anything supported by st.image, or None.
+    layout="centered", # Can be "centered" or "wide". In the future also "dashboard", etc.
+    initial_sidebar_state="expanded", # Can be "auto", "expanded", "collapsed"
+)
+
+try:
+    # Before Streamlit 0.65
+    from streamlit.ReportThread import get_report_ctx
+    from streamlit.server.Server import Server
+except ModuleNotFoundError:
+    # After Streamlit 0.65
+    from streamlit.report_thread import get_report_ctx
+    from streamlit.server.server import Server
+
+
+def main():
+    state = _get_state()
+    pages = {
+        "Home": project_explanation,
+        "Experiment": parameter_experiment,
+        "Hough Lines": hough_lines,
+        "Extrapolate Lines": extrapolate_lines,
+        "Stabilize Lines": stabilize_lines,
+        "Fill Lines": fill_lines
+    }
+
+    st.sidebar.title(":bookmark_tabs: Navigation")
+    
