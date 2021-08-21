ae945ff5685eb7e8140a8204b50242e57b6c4e60
Vamshi-Krishna-Prime <vamshi.krishna.prime@gmail.com>
2021-08-21 14:19:04 +0530

    Edit: app, functions description

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ param_max_line_gap = 70
 def grayscale(img):
     """Applies the Grayscale transform
     This will return an image with only one color channel
-    but NOTE: to see the returned image as grayscale
+    but Note: to see the returned image as grayscale
     (assuming your grayscaled image is called 'gray')
     you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -107,7 +107,7 @@ def region_of_interest(img, vertices):
 
 def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
     """
-    NOTE: this is the function you might want to use as a starting point once you want to 
+    Note: this is the function you might want to use as a starting point once you want to 
     average/extrapolate the line segments you detect to map out the full
     extent of the lane (going from the result shown in raw-lines-example.mp4
     to that shown in P1_example.mp4).  
@@ -221,20 +221,13 @@ def add_image_line(img, x1, y1, x2, y2, x3, color, thickness):
 
 def draw_lines_extrapolated(img, lines, color=[9, 219, 44], thickness=5):
     """
-    NOTE: this is the function you might want to use as a starting point once you want to 
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).  
-    
-    Think about things like separating line segments by their 
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of 
-    the lines and extrapolate to the top and bottom of the lane.
-    
-    This function draws `lines` with `color` and `thickness`.    
+    Note:
+    This function draws the extrapolated `lines` with `color` and `thickness`.
+    Removed redundant lines with extreme slopes.
+    Used Linear regression to fit the line from the points on each side of the lane.
     Lines are drawn on the image inplace (mutates the image).
     If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
+    this function with the weighted_img() in the function below.
     """
     
     global r_x1
@@ -374,20 +367,14 @@ def process_image_extrapolated(img):
 
 def draw_lines_extrapolated_stable(img, lines, color=[9, 219, 44], thickness=5):
     """
-    NOTE: this is the function you might want to use as a starting point once you want to 
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).  
-    
-    Think about things like separating line segments by their 
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of 
-    the lines and extrapolate to the top and bottom of the lane.
-    
-    This function draws `lines` with `color` and `thickness`.    
+    Note:
+    This function draws the extrapolated `lines` with `color` and `thickness`.
+    Removed redundant lines with extreme slopes.
+    Introduced a lerning rate to stabilize the lines with respect to previous values.
+    Used Linear regression to fit the line from the points on each side of the lane.
     Lines are drawn on the image inplace (mutates the image).
     If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
+    this function with the weighted_img() in the function below.
     """
     
     global r_x1
@@ -554,20 +541,15 @@ def add_image_fill(img, r_x1, r_x2, l_x1, l_x2, y1, y2, ignore_mask_color):
 
 def draw_lines_fill(img, lines, color=[9, 219, 44], thickness=5):
     """
-    NOTE: this is the function you might want to use as a starting point once you want to 
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).  
-    
-    Think about things like separating line segments by their 
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of 
-    the lines and extrapolate to the top and bottom of the lane.
-    
-    This function draws `lines` with `color` and `thickness`.    
-    Lines are drawn on the image inplace (mutates the image).
-    If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
+    Note:
+     This function draws the extrapolated `lines` with `color` and `thickness`.
+    Removed redundant lines with extreme slopes.
+    Introduced a lerning rate to stabilize the lines with respect to previous values.
+    Used Linear regression to fit the line from the points on each side of the lane.
+    Implemented Polyfit method to fill the area between the lines.
+    Area filled on the image is inplace (mutates the image).
+    If you want to make the area semi-transparent, think about combining
+    this function with the weighted_img() in the function below.
     """
     
     global r_x1
@@ -724,7 +706,8 @@ def main():
         "Hough Lines": hough_lines_page,
         "Extrapolate Lines": extrapolate_lines,
         "Stabilize Lines": stabilize_lines,
-        "Fill Lines": fill_lines
+        "Fill Lines": fill_lines,
+        "Credentials": credentials
     }
 
     st.sidebar.title(":bookmark_tabs: Navigation")
@@ -1960,6 +1943,11 @@ def fill_lines(state):
         video_bytes = video_file.read()
         st.video(video_bytes)
     
+
+
+def credentials(state):
+    st.markdown('# Credentials')
+    st.write('------------')
 
 
 
